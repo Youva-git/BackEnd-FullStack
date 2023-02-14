e97e2cb63c3bdf761b81d961c28496dc6b8ec01b
Youva Djellal <Youvadjellal@outlook.fr>
2023-02-14 18:57:24 +0100

    feat : gestion de categories

---
+++ src/main/java/com/fullstack/backend/service/CategorieService.java
@@ -0,0 +1,16 @@
+package com.fullstack.backend.service;
+
+import com.fullstack.backend.dto.CategorieDto;
+
+import java.util.List;
+
+public interface CategorieService {
+    CategorieDto create(CategorieDto categorie);
+    List<CategorieDto> read();
+    CategorieDto findById(Integer id);
+    CategorieDto findByNom(String codeBoutique);
+    CategorieDto update(Integer id, CategorieDto categorie);
+    String delete(Integer id);
+    String addCategorieToProduits(Integer idCategorie, List<Integer> idProduits);
+    List<CategorieDto> findByIdBoutique(Integer idBoutique);
+}
